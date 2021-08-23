959c54859a27d0f87a66a03c1a51596e3a537407
jonathan kerbrat <jonathan.kerbrat@hotmail.com>
2021-08-23 15:34:24 +0200

    controllers ok routes maj

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def user_params
+  end
+end
