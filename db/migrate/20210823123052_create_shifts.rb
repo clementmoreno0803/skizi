c9e86b9e2d4d7333af45d74c999c8228aa5f81dd
michael grosheny <grosheny.michael@gmail.com>
2021-08-23 14:37:53 +0200

    modif db

--- db/migrate/20210823123052_create_shifts.rb
+++ db/migrate/20210823123052_create_shifts.rb
@@ -5,7 +5,7 @@ class CreateShifts < ActiveRecord::Migration[6.1]
       t.date :ended_at
       t.string :post
       t.string :status
-      t.references :entities, null: false, foreign_key: true
+      t.references :entity, null: false, foreign_key: true
 
       t.timestamps
     end
