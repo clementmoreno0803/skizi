3978dcec04c2fe879928c99a392e2a46acee527b
jonathan kerbrat <jonathan.kerbrat@hotmail.com>
2021-08-23 16:18:33 +0200

    methodes primaires ok

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,22 +1,46 @@
 class ShiftsController < ApplicationController
+  before_action :find_dev, only: %i[show destroy edit update]
   def index
+    @shifts = Shift.all
   end
 
   def new
+    @shift = Shift.new
   end
 
   def create
+    @shift = Shift.new(shift_params)
+
+    if @shift.save
+      redirect_to shifts_path(@shift)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @shift.update(shift_params)
+      redirect_to shift_path(@shift)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @shift.destroy
+    redirect_to shifts_path
   end
 
-  def shit_params
+  private
+
+  def shift_params
+    params.require(:shift).permit(:started_at, :ended_at, :job, :status)
+  end
+
+  def find_shift
+    @shift = shift.find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,46 @@
 class UsersController < ApplicationController
+  before_action :find_dev, only: %i[show destroy edit update]
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path(@user)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @user.destroy
+    redirect_to users_path
   end
 
+  private
+
   def user_params
+    params.require(:user).permit(:username, :jobs, :email, :contract_hours_per_week)
+  end
+
+  def find_user
+    @user = user.find(params[:id])
   end
 end
