b4b5047f9f7a3255293c1a4bea5fb172233d5867
clement moreno <clementmoreno0803@gmail.com>
2021-08-23 14:32:01 +0200

    debut set up

---
+++ db/migrate/20210823123052_create_shifts.rb
@@ -0,0 +1,13 @@
+class CreateShifts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :shifts do |t|
+      t.date :started_at
+      t.date :ended_at
+      t.string :post
+      t.string :status
+      t.references :entities, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
