5e84c43ab1a510d237f94a59b152ce758a14ecb7
michael grosheny <grosheny.michael@gmail.com>
2021-08-23 16:16:29 +0200

    change date format

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_130049) do
+ActiveRecord::Schema.define(version: 2021_08_23_140052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 2021_08_23_130049) do
   end
 
   create_table "shifts", force: :cascade do |t|
-    t.date "started_at"
-    t.date "ended_at"
+    t.datetime "started_at", precision: 6, null: false
+    t.datetime "ended_at", precision: 6, null: false
     t.string "job"
     t.string "status"
     t.bigint "user_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Shift.destroy_all
+User.destroy_all
+Entity.destroy_all
+
+entity = Entity.create!(name: 'Le complexe du rire')
+
+user1 = User.create!(email: 'toto@gmail.com', password: 'tototo', password_confirmation: 'tototo', username: 'toto', entity_id: entity.id)
+
+Shift.create!(started_at: "2021,09,03,18,00,00", ended_at: "2021,09,03,21,00,00", job: 'runner', status: "Reclamation", user_id: user1.id, entity_id: entity.id)
