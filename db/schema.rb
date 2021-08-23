3b08729e7312edefff1e6dcd785200f41ccc022e
clement moreno <clementmoreno0803@gmail.com>
2021-08-23 20:06:24 +0200

    debut skizi

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_08_23_123052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "entities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shifts", force: :cascade do |t|
+    t.date "started_at"
+    t.date "ended_at"
+    t.string "post"
+    t.string "status"
+    t.bigint "entities_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["entities_id"], name: "index_shifts_on_entities_id"
+  end
+
+  add_foreign_key "shifts", "entities", column: "entities_id"
 end
