310d71e5759b8a7b0ddc4cf11fad4a71d5d5e769
michael grosheny <grosheny.michael@gmail.com>
2021-08-24 14:34:24 +0200

    seed for all models

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,5 +1,5 @@
 class Shift < ApplicationRecord
-  belongs_to :manager, class_name: "User", foreign_key: 'user_id'
+  belongs_to :manager, class_name: "User"
+  has_many :employees, through: :user_shifts, class_name: 'User'
   has_many :user_shifts
-  has_many :employees, through: :user_shifts, class_name: 'User', foreign_key: 'user_id'
 end
