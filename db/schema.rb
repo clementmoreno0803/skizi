43420867f61bbf535750b94d0b187d4d2a5291ad
clement moreno <clementmoreno0803@gmail.com>
2021-08-24 14:58:39 +0200

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_08_23_123052) do
-=======
-ActiveRecord::Schema.define(version: 2021_08_23_130049) do
->>>>>>> 93bf2f40284d06605e17ddbace0c922e74a06583
-=======
 ActiveRecord::Schema.define(version: 2021_08_24_101036) do
->>>>>>> 5029e1faa1724a1a3982e00f564bb7925a6b80a9
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,26 +27,9 @@ ActiveRecord::Schema.define(version: 2021_08_24_101036) do
     t.index ["manager_id"], name: "index_shifts_on_manager_id"
   end
 
-<<<<<<< HEAD
-  create_table "shifts", force: :cascade do |t|
-    t.date "started_at"
-    t.date "ended_at"
-<<<<<<< HEAD
-    t.string "post"
-    t.string "status"
-    t.bigint "entities_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["entities_id"], name: "index_shifts_on_entities_id"
-  end
-
-  add_foreign_key "shifts", "entities", column: "entities_id"
-=======
-=======
   create_table "user_shifts", force: :cascade do |t|
     t.bigint "employee_id", null: false
     t.bigint "shift_id", null: false
->>>>>>> 5029e1faa1724a1a3982e00f564bb7925a6b80a9
     t.string "job"
     t.integer "hours_worked"
     t.datetime "created_at", precision: 6, null: false
@@ -78,13 +53,7 @@ ActiveRecord::Schema.define(version: 2021_08_24_101036) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
-  add_foreign_key "shifts", "entities"
-  add_foreign_key "users", "entities"
->>>>>>> 93bf2f40284d06605e17ddbace0c922e74a06583
-=======
   add_foreign_key "shifts", "users", column: "manager_id"
   add_foreign_key "user_shifts", "shifts"
   add_foreign_key "user_shifts", "users", column: "employee_id"
->>>>>>> 5029e1faa1724a1a3982e00f564bb7925a6b80a9
 end
