bb47ed083094cf5115cfd82e7bff30639d9364e9
michael grosheny <grosheny.michael@gmail.com>
2021-08-25 11:56:27 +0200

    status set in form and default value

--- db/migrate/20210825091851_add_status_to_user.rb
+++ db/migrate/20210825091851_add_status_to_user.rb
@@ -1,5 +1,5 @@
 class AddStatusToUser < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :status, :string
+    add_column :users, :status, :string, default: 'Ongoing'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2021_08_25_091851) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "status"
+    t.string "status", default: "Ongoing"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
