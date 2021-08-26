15328dacad91280b8494308ba0105c2f5cd63d1e
clement moreno <clementmoreno0803@gmail.com>
2021-08-26 14:21:25 +0200

    titre et soustitre + searchbar index user

--- app/javascript/components/search_bar.js
+++ app/javascript/components/search_bar.js
@@ -1,3 +1,5 @@
+document.addEventListener('turbolinks:load', () => {
+
 let icon = document.querySelector(".search_icon")
 let search_bar = document.querySelector(".search_bar")
 let scaled_sidebar = document.querySelector(".modif_manuelle")
@@ -6,4 +8,15 @@ console.log(scaled_sidebar)
 icon.addEventListener("click", (event) => {
   scaled_sidebar.classList.toggle('active');
   search_bar.classList.toggle('active');
+});
+});
+
+document.addEventListener('turbolinks:load', () => {
+
+  let input = document.querySelector("#my_search_index")
+  let search_bar_index = document.querySelector(".search_bar")
+
+  input.addEventListener("click", () =>{
+    search_bar_index.classList.toggle('active')
+  })
 });
