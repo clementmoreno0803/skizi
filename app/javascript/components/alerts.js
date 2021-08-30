6374e59ba50162df1cf3e031e6626175c62dd5c5
clement moreno <clementmoreno0803@gmail.com>
2021-08-30 17:07:12 +0200

    alerts

--- app/javascript/components/alerts.js
+++ app/javascript/components/alerts.js
@@ -1,15 +1,25 @@
-const almost_finish = document.querySelector('#almost');
-const finish = document.querySelector('#danger');
-const display_none = document.querySelector('#danger');
 
-let alerts = function (num1, num2) {
-  if (num1 >= 0.75 * num2 && num1 < 0.9 * num2) {
-   almost_finish.style.display = block;
-  }
-  else if (num1 > 0.9 * num2){
-    finish.style.display = block;
-  }
-  else {
+const initAlertOfWorkingTime = () => {
+  const almost_finish = document.querySelector('.almost');
+  const finish = document.querySelector('.danger');
 
-  }
+    const alerts = function (num1, num2) {
+      if (num1 >= 0.75 * num2 && num1 < 0.9 * num2) {
+        almost_finish.style.display = "block";
+        finish.style.display = "none";
+        console.log(almost_finish)
+      }
+      else if (num1 >= 0.9 * num2){
+        finish.style.display = "block";
+        almost_finish.style.display = "none";
+        console.log(finish)
+      }
+      else {
+        almost_finish.style.display = "none";
+        finish.style.display = "none";
+      }
+    }
+    alerts(20,25);
 }
+
+export default initAlertOfWorkingTime
--- app/javascript/plugins/calendar.js
+++ app/javascript/plugins/calendar.js
@@ -1,90 +1,84 @@
-import Calendar from 'tui-calendar';
-import "tui-calendar/dist/tui-calendar.css";
-import 'tui-date-picker/dist/tui-date-picker.css';
-import 'tui-time-picker/dist/tui-time-picker.css';
-import {MONTHLY_CUSTOM_THEME } from './templates';
-import {templates} from './templates';
-
-const initCalendarDayAndMonth = () => {
-  const calendar = new Calendar('#calendar-month', {
-    defaultView: 'month',
-    useCreationPopup: true,
-    useDetailPopup: true,
-    scheduleView: ['time'],
-    theme: MONTHLY_CUSTOM_THEME,
-    template: {
-      monthDayname: function (dayname) {
-        return '<span class="calendar-month-dayname-name">' + dayname.label + '</span>';
-      },
-      templates
-    }
-  });
-};
-
-const calendar2 = new Calendar('#calendar-day', {
-  defaultView: 'day',
-  taskView: ['task'],
-  useCreationPopup: true,
-  useDetailPopup: true,
-  scheduleView: ['time'],
-  week:{
-    hourStart: 9,
-    hourEnd: 22
-  },
-  template: {
-    Dayname: function (dayname) {
-      return '<span class="calendar-dayname-name">' + dayname.label + '</span>';
-    },
-    templates
-  }
-});
+// import Calendar from 'tui-calendar';
+// import "tui-calendar/dist/tui-calendar.css";
+// import 'tui-date-picker/dist/tui-date-picker.css';
+// import 'tui-time-picker/dist/tui-time-picker.css';
+// import {MONTHLY_CUSTOM_THEME } from './templates';
+// import {templates} from './templates';
 
 
-function RandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+// const calendar = new Calendar('#calendar-month', {
+//   defaultView: 'month',
+//   useCreationPopup: true,
+//   useDetailPopup: true,
+//   scheduleView: ['time'],
+//   theme: MONTHLY_CUSTOM_THEME,
+//   template: {
+//     monthDayname: function (dayname) {
+//       return '<span class="calendar-month-dayname-name">' + dayname.label + '</span>';
+//     },
+//     templates
+//   }
+// });
 
-// entries calendar
-const call = document.getElementById("calendar-month")
-const user_shifts = JSON.parse(call.dataset.user_shifts);
-user_shifts.forEach((i) => {
-  calendar.createSchedules([
-    {
-      id: `${i.id}`,
-      calendarId: '1',
-      title: `${i.title}`,
-      category: 'time',
-      dueDateClass: '',
-      start: `${i.start}`,
-      end: `${i.end}`,
-      bgColor: RandomColor
-    },
-  ])
-});
+// const calendar2 = new Calendar('#calendar-day', {
+//   defaultView: 'day',
+//   taskView: ['task'],
+//   useCreationPopup: true,
+//   useDetailPopup: true,
+//   scheduleView: ['time'],
+//   week:{
+//     hourStart: 9,
+//     hourEnd: 22
+//   },
+//   template: {
+//     Dayname: function (dayname) {
+//       return '<span class="calendar-dayname-name">' + dayname.label + '</span>';
+//     },
+//     templates
+//   }
+// });
+// function RandomColor() {
+//   var letters = '0123456789ABCDEF';
+//   var color = '#';
+//   for (var i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return color;
+// }
 
-user_shifts.forEach((i) => {
-  // console.log(i.id.string), console.log(i.title), console.log(i.job), console.log(i.start), console.log(i.end), console.log('1')
-  calendar2.createSchedules([
-    {
-      id: i.id,
-      calendarId: '1',
-      title: i.title,
-      category: 'time',
-      dueDateClass: '',
-      start: `${i.start}`,
-      end: `${i.end}`,
-      bgColor: RandomColor
-    }
-  ])
-});
+// // entries calendar
+// //   const call = document.getElementById("calendar-month")
+// //   const user_shifts = JSON.parse(call.dataset.user_shifts);
+// //   user_shifts.forEach((i) => {
+// //     calendar.createSchedules([
+// //       {
+// //         id: `${i.id}`,
+// //         calendarId: '1',
+// //         title: `${i.title}`,
+// //         category: 'time',
+// //         dueDateClass: '',
+// //         start: `${i.start}`,
+// //         end: `${i.end}`,
+// //         bgColor: RandomColor
+// //       },
+// //     ])
+// //   });
 
+// // user_shifts.forEach((i) => {
+// //   // console.log(i.id.string), console.log(i.title), console.log(i.job), console.log(i.start), console.log(i.end), console.log('1')
+// //   calendar2.createSchedules([
+// //     {
+// //       id: i.id,
+// //       calendarId: '1',
+// //       title: i.title,
+// //       category: 'time',
+// //       dueDateClass: '',
+// //       start: `${i.start}`,
+// //       end: `${i.end}`,
+// //       bgColor: RandomColor
+// //     }
+// //   ])
+// // });
 
-
-export {calendar2}
-export {calendar}
-export {initCalendarDayAndMonth}
+// export {calendar2}
+// export {calendar}
