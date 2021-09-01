cd9e671e083981ae85df83e6f12cd7a78abe0b21
clement moreno <clementmoreno0803@gmail.com>
2021-09-01 15:20:06 +0200

    resolution js

--- app/javascript/components/search_bar.js
+++ app/javascript/components/search_bar.js
@@ -1,30 +1,39 @@
 const initSearchBar = (Rails) => {
   // right sidebar
   const icon = document.querySelector(".search_icon")
-  const search_bar = document.querySelector(".search_bar")
+
+  // return if we are not on the shift index
+  if (icon) {
+      const search_bar = document.querySelector(".search_bar")
   const scaling_calendar = document.querySelector(".calendar");
   const text_field_shift = document.querySelector("#query")
   const text_field_user = document.querySelector("#name")
   const scaled_sidebar = document.querySelector(".modif_manuelle")
   const form = document.querySelector(".search_bar form")
 
-  // return if we are not on the shift index
-  if (icon) {
     icon.addEventListener("click", (event) => {
-      scaled_sidebar.classList.toggle('active');
+      if (scaled_sidebar) {
+        scaled_sidebar.classList.toggle('active');
+
+      }
       search_bar.classList.toggle('active');
-      scaling_calendar.classList.toggle('open')
+      // scaling_calendar.classList.toggle('open')
     });
 
-    text_field_shift.addEventListener("input", function (e) {
-      Rails.fire(form, 'submit')
-    })
-  }
+    if (text_field_shift) {
+
+      text_field_shift.addEventListener("input", function (e) {
+        Rails.fire(form, 'submit')
+      })
+    }
+
+
 
   if (text_field_user) {
     text_field_user.addEventListener("input", function (e) {
       Rails.fire(form, 'submit')
     })
+  }
   }
 }
 
